76ff378ec9b9102dc490ca4356f43fed7e2479d1
AlexanderMelde <2115644+AlexanderMelde@users.noreply.github.com>
2018-11-06 00:35:57 +0100

    sound analysis with easier curve

--- helpers/sound_analysis.py
+++ helpers/sound_analysis.py
@@ -2,6 +2,7 @@ import scipy
 import scipy.signal
 import subprocess
 import matplotlib.pyplot as plt
+import pandas as pd
 import numpy as np
 
 wavfile = "../input_ex.wav"
@@ -17,15 +18,22 @@ print("[i] Das Eingangssignal hat eine Rate von",fs,"samples/sec. Bei",
 
 #T=Transpose: Signal-Achsen vertauschen und dann die erste Reihe nehmen
 signal = sig.T[0]
-
-yhat = scipy.signal.savgol_filter(signal, 51, 3) # window size 51, polynomial order 3
+signal = abs(signal)
+print(signal.shape)
+print(signal)
+pdsig = pd.Series(signal)
+pdsig_r = pdsig.rolling(window=900000)
+pdsig_r_mean = pdsig_r.mean()
+pdsig_r_mean.plot(style='k')
+#sig2 = np.convolve(signal,np.ones(30000,dtype=int),'valid')
+#yhat = scipy.signal.savgol_filter(signal, 51, 3) # window size 51, polynomial order 3
 
 ##################
 
 # Zeitsignale plotten
 plt.figure(figsize=(15,5),facecolor='w')
 plt.plot(signal, label='Original Input Signal')
-plt.plot(yhat, label='Original Input Signal')
+plt.plot(pdsig_r_mean , label='Original Input Signal')
 plt.title("Zeitsignale für Aufgabe 1 (überlappend)",fontsize="xx-large", y=1.12)
 plt.xlim(0, len(signal))
 #plt.ylim(-1, 1)
