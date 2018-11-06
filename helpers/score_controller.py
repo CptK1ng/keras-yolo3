3d80471bd6f97fc96e787c51f90664970929dda2
AlexanderMelde <2115644+AlexanderMelde@users.noreply.github.com>
2018-11-06 10:34:54 +0100

    score wird berechnet

--- helpers/score_controller.py
+++ helpers/score_controller.py
@@ -10,16 +10,38 @@ class ScoreController(object):
         self.frames = frames
 
     def get_feel_good_level(self, framenr=None):
-        if framenr is None:
-            return len(self.action_db.filter())
-        else:
-            return False # else calc frame level score
+        return 1-self.get_threat_level(framenr)
+        #if framenr is None:
+        #    return len(self.action_db.filter([]))
+        #else:
+        #    return False # else calc frame level score
 
     def get_threat_level(self, framenr=None):
         if framenr is None:
             return len(self.action_db.actions)#+self.frames.nr_of_cars
         else:
-            return False  # else calc frame level score
+            nr_of_persons = 0
+            nr_of_groups = 0
+            nr_of_cars = 0
+            nr_of_warning_zones = 0
+            nr_of_danger_zones = 0
+            avg_person_speed = 4
+            nr_of_falling = 0
+            isHighVol = False
+            time_day = True
+            if time_day:
+                score = int(nr_of_persons >= 100)*0.15+int(nr_of_groups >= 10)*0.05\
+                    +int(nr_of_cars >= 1)*0.2+int(nr_of_warning_zones >= 5)*0.05\
+                    +int(nr_of_danger_zones >= 1)*0.1+int(avg_person_speed>=6)*0.1\
+                    +int(nr_of_falling>=1)*0.25+int(isHighVol)*0.1
+            else:
+                score = int(nr_of_persons >= 30 or nr_of_persons <= 5) * 0.15 \
+                    +int(nr_of_groups >= 1)*0.15\
+                    +int(nr_of_cars >= 1)*0.2+int(nr_of_warning_zones >= 1)*0.05\
+                    +int(nr_of_danger_zones >= 1)*0.1+int(avg_person_speed>=6)*0.05\
+                    +int(nr_of_falling>=1)*0.2+int(isHighVol)*0.15
+
+            return score
 
 
 def test_sc():
@@ -37,5 +59,9 @@ def test_sc():
     print("FEEL GOOD LEVEl: ", sc.get_feel_good_level())
     print("THREAD LEVEl: ", sc.get_threat_level())
 
+    print("IN FRAME 30")
+    t = sc.get_threat_level(30)
+    print("FEEL GOOD LEVEl: ", 1-t)
+    print("THREAD LEVEl: ", t)
 
-#test_sc()
+test_sc()
